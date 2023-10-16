d69f8920aae1396df492d8ebff80c2726591c993
MalikGMoroz <148124706+MalikGMoroz@users.noreply.github.com>
2023-10-16 20:52:02 +0300

    Update main.py

--- HW6/main.py
+++ HW6/main.py
@@ -1,1 +1,53 @@
+from pathlib import Path
+import shutil
+import sys
+import file_parser as parser
+from normalize import normalize
+
+def handle_media(filename: Path, target_folder: Path):
+    target_folder.mkdir(exist_ok=True, parents=True)
+    shutil.move(str(filename), str(target_folder / normalize(filename.name)))
+
+def handle_other(filename: Path, target_folder: Path):
+    target_folder.mkdir(exist_ok=True, parents=True)
+    shutil.move(str(filename), str(target_folder / normalize(filename.name)))
+
+def handle_archive(filename: Path, target_folder: Path):
+    target_folder.mkdir(exist_ok=True, parents=True)
+    folder_for_file = target_folder / normalize(filename.name.replace(filename.suffix, ''))
+    folder_for_file.mkdir(exist_ok=True, parents=True)
+    try:
+        shutil.unpack_archive(str(filename.resolve()), str(folder_for_file.resolve()))
+    except shutil.ReadError:
+        print(f'{filename} is not an archive!')
+        folder_for_file.rmdir()
+        return None
+    filename.unlink()
+
+def handle_folder(folder: Path):
+    try:
+        folder.rmdir()
+    except OSError:
+        print(f'Failed to delete folder {folder}')
+
+def main(folder: Path):
+    parser.scan(folder)
+
+    for file in parser.JPEG_IMAGES:
+        handle_media(file, folder / 'images' / 'JPEG')
+    # ... [similar lines for other file types are omitted for brevity]
+
+    for file in parser.ARCHIVES:
+        handle_archive(file, folder / 'archives')
+
+    for folder in parser.FOLDERS[::-1]:
+        handle_folder(folder)
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        folder_for_scan = Path(sys.argv[1])
+        print(f'Start in folder {folder_for_scan.resolve()}')
+        main(folder_for_scan.resolve())
+    else:
+        print("Please provide the folder to be scanned as an argument.")
 
